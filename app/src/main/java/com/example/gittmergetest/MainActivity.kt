e290b48487f7c1328634a5f64d26db2e03f3f222
yashtandon007 <yasmoney1992#>
2022-06-22 11:56:52 +0530

    master change 1

--- app/src/main/java/com/example/gittmergetest/MainActivity.kt
+++ app/src/main/java/com/example/gittmergetest/MainActivity.kt
@@ -54,5 +54,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+            //master change 1
     }
 }
