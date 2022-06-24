9ff65a2f83fc18519bc9c6549438d7fe93a7d794
yashtandon007 <yashtandon007@users.noreply.github.com>
2022-06-24 13:25:58 +0530

    child

--- app/src/main/java/com/example/gittmergetest/MainActivity.kt
+++ app/src/main/java/com/example/gittmergetest/MainActivity.kt
@@ -54,6 +54,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-      //parent
+      //child
     }
 }
