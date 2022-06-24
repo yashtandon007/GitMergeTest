20bcdd1ebe8a51dcb2909fb80c99cdff61cd94ba
yashtandon007 <yashtandon007@users.noreply.github.com>
2022-06-24 13:54:44 +0530

    //baap

--- app/src/main/java/com/example/gittmergetest/MainActivity.kt
+++ app/src/main/java/com/example/gittmergetest/MainActivity.kt
@@ -54,6 +54,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-      //beta 3
+      //beta 4
     }
 }
